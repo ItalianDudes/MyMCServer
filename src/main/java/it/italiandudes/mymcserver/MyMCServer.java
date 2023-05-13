7754ffaebaa35db886eae93bd131430b6ae366d3
Hacker6329 <Hacker6329@gmail.com>
2023-05-14 00:59:58 +0200

    Added connection module and dbconnection module to enable and disable routin

--- src/main/java/it/italiandudes/mymcserver/MyMCServer.java
+++ src/main/java/it/italiandudes/mymcserver/MyMCServer.java
@@ -2,11 +2,17 @@ package it.italiandudes.mymcserver;
 
 import it.italiandudes.idl.common.StringHandler;
 import it.italiandudes.mymcserver.exceptions.ModuleException;
+import it.italiandudes.mymcserver.exceptions.modules.ModuleMissingConfigsException;
+import it.italiandudes.mymcserver.exceptions.modules.ModuleMissingDependenciesException;
 import it.italiandudes.mymcserver.modules.CommandsModule;
 import it.italiandudes.mymcserver.modules.ConfigModule;
+import it.italiandudes.mymcserver.modules.DBConnectionModule;
 import it.italiandudes.mymcserver.modules.LocalizationModule;
+import it.italiandudes.mymcserver.modules.connection.ConnectionModule;
 import it.italiandudes.mymcserver.utils.Defs;
+import it.italiandudes.mymcserver.utils.Resource;
 import it.italiandudes.mymcserver.utils.ServerLogger;
+import org.bukkit.Server;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
@@ -35,10 +41,16 @@ public final class MyMCServer extends JavaPlugin {
             // Load Langs from Config's selected Lang
             loadLangs(this);
 
+            // Load the DBConnection module using configs
+            loadDB();
+
             ServerLogger.getLogger().info("All core modules are loaded, loading plugin modules...");
 
             // Load Commands
             CommandsModule.load(this);
+
+            // Load Connection using DBConnection and configs
+            loadConnection();
 
         }catch (Exception e) {
             ServerLogger.getLogger().severe("An unhandled exception has reached the function, shutting down the plugin...");
@@ -53,6 +65,9 @@ public final class MyMCServer extends JavaPlugin {
     public void onDisable() {
         if (instanceErrored) return;
         instanceErrored = true;
+        try {
+            ConnectionModule.unload(true);
+        } catch (ModuleException ignored) {}
         try {
             CommandsModule.unload(true);
         } catch (ModuleException ignored) {}
@@ -72,6 +87,10 @@ public final class MyMCServer extends JavaPlugin {
     public static JavaPlugin getPluginInstance() {
         return pluginInstance;
     }
+    @NotNull
+    public static Server getServerInstance() {
+        return pluginInstance.getServer();
+    }
 
     // Methods
     private void loadConfigs(@NotNull JavaPlugin pluginInstance) throws ModuleException {
@@ -79,5 +98,20 @@ public final class MyMCServer extends JavaPlugin {
     }
     private void loadLangs(@NotNull JavaPlugin pluginInstance) throws ModuleException {
         LocalizationModule.load(pluginInstance, Objects.requireNonNull(ConfigModule.getConfig(Defs.Config.Identifiers.GENERAL_CONFIG, Defs.Config.Keys.General.KEY_LANG)));
+    }
+    private void loadDB() throws ModuleException {
+        try {
+            DBConnectionModule.load(Objects.requireNonNull(ConfigModule.getConfig(Defs.Config.Identifiers.GENERAL_CONFIG, Defs.Config.Keys.General.KEY_DATABASE_URL)));
+        } catch (ModuleMissingConfigsException e) {
+            return;
+        }
+            String dbType = DBConnectionModule.getConnectorType();
+            String query = DBConnectionModule.getQueryFromResourcesFileSQL(Resource.Path.DBConnection.SQL_DIR + dbType + Resource.Path.DBConnection.SQL_FILE_EXTENSION);
+            DBConnectionModule.executePreparedStatementFromQueryIgnoreResult(query);
+    }
+    private void loadConnection() throws ModuleException {
+        try {
+            ConnectionModule.load(Integer.parseInt(Objects.requireNonNull(ConfigModule.getConfig(Defs.Config.Identifiers.GENERAL_CONFIG, Defs.Config.Keys.General.KEY_CONNECTION_PORT))));
+        } catch (ModuleMissingDependenciesException ignored) {}
     }
 }
